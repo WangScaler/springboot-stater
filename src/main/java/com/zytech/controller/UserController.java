3a4ed8a298932ea5b4bb3f56acc3bb7a019d5f53
“WangScaler <WangScaler@163.com>
2021-02-20 10:59:06 +0800

    集成redis

--- src/main/java/com/zytech/controller/UserController.java
+++ src/main/java/com/zytech/controller/UserController.java
@@ -7,6 +7,8 @@ import com.zytech.service.UserService;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,17 +26,26 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
 
     @RequiresAuthentication
     @RequiresRoles("admin")
     @GetMapping("/index")
     public Result index() {
+        ValueOperations<String,String> operations = redisTemplate.opsForValue();
+        operations.set("hello","world");
+        String hello = operations.get("hello");
+        System.out.println(hello);
         User user = userService.findSimpleUserInfoById(1L);
+
         return Result.succ(user);
     }
-
+    @RequiresAuthentication
+    @RequiresRoles("admin")
     @PostMapping("/save")
     public Result save(@Validated @RequestBody User user) {
+        userService.saveUser(user);
         return Result.succ(user);
     }
 
