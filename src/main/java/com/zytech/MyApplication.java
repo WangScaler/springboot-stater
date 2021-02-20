a300e510c23629507573f3f775456d98cb02a3ea
“WangScaler <WangScaler@163.com>
2021-02-20 16:43:36 +0800

    开启swagger

--- src/main/java/com/zytech/MyApplication.java
+++ src/main/java/com/zytech/MyApplication.java
@@ -2,9 +2,9 @@ package com.zytech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
-//@EnableOpenApi
+@EnableOpenApi
 @SpringBootApplication
 public class MyApplication {
 
